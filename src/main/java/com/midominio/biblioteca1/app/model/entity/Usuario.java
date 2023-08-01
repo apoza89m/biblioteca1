53095eeb3638e6f45f2bc03aaf073807ae993384
robe <robe@example.com>
2023-08-01 14:25:21 +0200

    a

--- src/main/java/com/midominio/biblioteca1/app/model/entity/Usuario.java
+++ src/main/java/com/midominio/biblioteca1/app/model/entity/Usuario.java
@@ -2,7 +2,6 @@ package com.midominio.biblioteca1.app.model.entity;
 
 import java.io.Serializable;
 import java.time.LocalDate;
-import java.util.Collection;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
@@ -11,8 +10,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import jakarta.validation.constraints.Digits;
-import jakarta.validation.constraints.Digits.List;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
